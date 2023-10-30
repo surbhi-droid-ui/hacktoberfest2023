1e1697f048a6ac8279270872d6e72f2a9ac358f4
Rohit <34805019+Lonewolf22xx@users.noreply.github.com>
2023-10-30 15:08:41 +0530

    Update snake.cpp

--- snake.cpp
+++ snake.cpp
@@ -17,7 +17,69 @@ namespace Snake
             int screenwidth = Console.WindowWidth;
             int screenheight = Console.WindowHeight;
             //unwanted code removed
-            int score = 5;
+            int score = 5;t evaluate(char b[3][3]) 
+{ 
+	// Checking for Rows for X or O victory. 
+	for (int row = 0; row < 3; row++) { 
+		if (b[row][0] == b[row][1] 
+			&& b[row][1] == b[row][2]) { 
+			if (b[row][0] == player) 
+				return +10; 
+			else if (b[row][0] == opponent) 
+				return -10; 
+		} 
+	} 
+
+	// Checking for Columns for X or O victory. 
+	for (int col = 0; col < 3; col++) { 
+		if (b[0][col] == b[1][col] 
+			&& b[1][col] == b[2][col]) { 
+			if (b[0][col] == player) 
+				return +10; 
+
+			else if (b[0][col] == opponent) 
+				return -10; 
+		} 
+	} 
+
+	// Checking for Diagonals for X or O victory. 
+	if (b[0][0] == b[1][1] && b[1][1] == b[2][2]) { 
+		if (b[0][0] == player) 
+			return +10; 
+		else if (b[0][0] == opponent) 
+			return -10; 
+	} t evaluate(char b[3][3]) 
+{ 
+	// Checking for Rows for X or O victory. 
+	for (int row = 0; row < 3; row++) { 
+		if (b[row][0] == b[row][1] 
+			&& b[row][1] == b[row][2]) { 
+			if (b[row][0] == player) 
+				return +10; 
+			else if (b[row][0] == opponent) 
+				return -10; 
+		} 
+	} 
+
+	// Checking for Columns for X or O victory. 
+	for (int col = 0; col < 3; col++) { 
+		if (b[0][col] == b[1][col] 
+			&& b[1][col] == b[2][col]) { 
+			if (b[0][col] == player) 
+				return +10; 
+
+			else if (b[0][col] == opponent) 
+				return -10; 
+		} 
+	} 
+
+	// Checking for Diagonals for X or O victory. 
+	if (b[0][0] == b[1][1] && b[1][1] == b[2][2]) { 
+		if (b[0][0] == player) 
+			return +10; 
+		else if (b[0][0] == opponent) 
+			return -10; 
+	} 
             int gameover = 0;
             pixel hoofd = new pixel();
             hoofd.xpos = screenwidth/2;
